a3792e0aa179faf7f3e1b83197d009a7c12e50cf
AntonyLaurente2002 <antony.laurente@tecsup.edu.pe>
2023-10-06 15:26:03 -0500

    Subir cambios

--- Laboratorio05/Laboratorio05/ButtonDemo.xaml.cs
+++ Laboratorio05/Laboratorio05/ButtonDemo.xaml.cs
@@ -14,33 +14,8 @@ namespace Laboratorio05
     {
         public ButtonDemo()
         {
-            InitializeComponent();
-
-            Title = "Code Button Click";
-
-            Label label = new Label
-            {
-                Text = "Click the Button below",
-                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
-                VerticalOptions = LayoutOptions.CenterAndExpand,
-                HorizontalOptions = LayoutOptions.Center
-            };
-
-            Button button = new Button
-            {
-                Text = "Click to rotate Text!",
-                VerticalOptions = LayoutOptions.CenterAndExpand,
-                HorizontalOptions = LayoutOptions.Center
-            };
-            button.Clicked += async (sender, args) => await label.RelRotateTo(360, 1000);
-            Content = new StackLayout
-            {
-                Children =
-                {
-                    label,
-                    button
-                }
-            };
+            InitializeComponent();           
+            
         }
     }
 }
